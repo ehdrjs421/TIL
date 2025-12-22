fbba413fbc32357e404fbdacaa2a0bd1a42f2c06
ehdrjs421 <ehdrjs421@gmail.com>
2025-12-22 17:48:44 +0900

    "[add] python.md"

--- python_practice/practice.py
+++ python_practice/practice.py
@@ -1,4 +1,32 @@
-a = "word"
-b = str(4)
-c = a + " is " + b
-print(c)
+# a = "word"
+# b = str(4)
+# c = a + " is " + b
+# print(c)
+
+# #/////////////////
+# #소수 판별시 사용
+# for i in (2):
+#         check = 0
+#         for j in range(2,int(i**(1/2))+1):
+#             if(i % j == 0):
+#                 check = 1 
+#                 break
+#         if (check == 0):
+#             answer += 1
+#             print(i)
+
+# # 딕셔너리 기능확인  
+# a = {}
+# print(len(a))
+# for i in range(2):
+#     a[i] = 3
+# for i in range(2):
+#     for j in a:
+#         if (i == j):
+#             a[i] -= i
+# print(a)
+# print(len(a))
+
+# # 리스트 기능 확인
+a =[1,3,2,4]
+print(min(a))
